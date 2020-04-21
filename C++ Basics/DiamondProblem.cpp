b40c2380d0375e66ea2b834da2fda2a60cdbe0a0
swapnil341995 <sbharude.3@gmail.com>
2020-04-22 01:09:28 +0530

    Diamond problem

---
+++ C++ Basics/DiamondProblem.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+class grandParent{
+    public :
+    grandParent(){
+        cout<<"Inside grandparent constructor."<<endl;
+    }
+};
+class parent1 : virtual public grandParent{
+    public :
+    parent1(){
+        cout<<"inside parent1 constructor!!!"<<endl;
+    }
+};
+class parent2 : virtual public grandParent{
+    public :
+    parent2(){
+        cout<<"inside parent2 constructor"<<endl;
+    }
+};
+class child : public parent1,parent2
+{
+    public :
+    child(){
+        cout<<"in child class constructor"<<endl;
+    }
+};
+int main(){
+    child c1;   //to solve the diamond problem lets make 
+}
